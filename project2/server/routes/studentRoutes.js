835ae78c2336ae01c0d2f5126f55eac5ac97ec9c
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-06-25 23:24:55 +0530

    Add student routes and controller integration for CRUD operations

---
+++ project2/server/routes/studentRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} = require("../controllers/studentController");
+
+router.post("/", createStudent);
+router.get("/", getAllStudents);
+router.get("/:id", getStudentById);
+router.put("/:id", updateStudent);
+router.delete("/:id", deleteStudent);
+module.exports = router;
