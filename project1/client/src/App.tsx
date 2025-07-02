a4a5d39d10a478f030d4f1d56ac3074fc12ceee4
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-07-02 09:47:32 +0530

    Implement Classroom Dashboard with StudentCard component and basic counter functionality

--- project1/client/src/App.tsx
+++ project1/client/src/App.tsx
@@ -1,31 +1,35 @@
 import "./App.css";
-import axios from "axios";
 import { useState, useEffect } from "react";
+import StudentCard from "./components/StudentCard";
 
-async function fetchData() {
-  const response = await axios.post("http://localhost:3000/", {
-    body: {
-      name: "John Doe",
-      age: 30,
-      city: "New York",
-    },
-  });
-  return response.data;
-}
-
-function App() {
-  const [data, setData] = useState(null);
+// async function fetchData() {
+//   const response = await axios.post("http://localhost:3000/", {
+//     body: {
+//       name: "John Doe",
+//       age: 30,
+//       city: "New York",
+//     },
+//   });
+//   return response.data;
+// }
+export default function HelloWorld() {
+  const [count, setcount] = useState(0);
   useEffect(() => {
-    fetchData()
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-  console.log("Data fetched:", data);
-  return <>Hello world!</>;
+    console.log("useEffect called");
+  }, [count]);
+  return (
+    <div>
+      <h1> Classroom Dashboard</h1>
+      <StudentCard name="John Doe" grade="A" />
+      <StudentCard name="Jane Smith" grade="B" />
+      <StudentCard name="Alice Johnson" grade="C" />
+      <StudentCard name="Bob Brown" grade="B+" />
+      <p>Count: {count}</p>
+      <div className="gap-2 flex ">
+        <button onClick={() => setcount(count + 1)}>Increment</button>
+        <button onClick={() => setcount(count - 1)}>Decrement</button>
+        <button onClick={() => setcount(0)}>Reset</button>
+      </div>
+    </div>
+  );
 }
-
-export default App;
